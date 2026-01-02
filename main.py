e4999f081d65930245801af629d0805dbd3e3a20
klpkt <metareibo@gmail.com>
2026-01-02 06:50:35 +0100

    add formatted printout

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
-from stats import count_words, count_chars
+import sys
+from stats import count_words, count_chars, sort_counts
 
 def get_book_text(filename):
     with open(filename) as f:
         return f.read()
 
 def main():
-    file_contents = get_book_text("books/frankenstein.txt")
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    filename = sys.argv[1]
+
+    file_contents = get_book_text(filename)
     num_words = count_words(file_contents)
-    print(f"Found {num_words} total words")
     chars = count_chars(file_contents)
-    print(chars)
+    sorted_chars = sort_counts(chars)
+
+    print( "============ BOOKBOT ============")
+    print(f"Analyzing  book found at {filename}")
+    print( "----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print( "--------- Character Count -------")
+    for item in sorted_chars:
+        if not item["char"].isalpha(): continue
+        print(f"{item["char"]}: {item["num"]}")
 
 main()
--- stats.py
+++ stats.py
@@ -14,3 +14,16 @@ def count_chars(text):
     for c in lowered:
         counts[c] = counts.get(c, 0) + 1
     return counts
+
+def sort_on(items):
+    return items["num"]
+
+def sort_counts(counts):
+    sorted_counts = []
+    for key in counts:
+        sorted_counts.append({
+            "char": key,
+            "num": counts[key]
+        })
+    sorted_counts.sort(reverse=True, key=sort_on)
+    return sorted_counts
